a0363e32861ec8d43b05705e08bb0b375aa93f1f
robus1993 <rsukhudyan@gmail.com>
2026-03-03 17:11:34 -0800

    Fix clear data migration (remove BEGIN/COMMIT for Webflow D1)

--- migrations/0007_clear_all_data.sql
+++ migrations/0007_clear_all_data.sql
@@ -1,8 +1,8 @@
 ﻿-- 0007_clear_all_data.sql
-BEGIN;
+-- Clear all DB rows (Webflow/WXP D1 migrations do NOT allow BEGIN/COMMIT/SAVEPOINT)
+
 DELETE FROM payments;
 DELETE FROM invoices;
 DELETE FROM assignments;
 DELETE FROM projects;
 DELETE FROM vendors;
-COMMIT;
